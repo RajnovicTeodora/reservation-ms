34e0baeb44544845e91c95105ce416b6e103f62e
Rajtarov Natasa <rajtarovnatasa@gmail.com>
2023-05-31 16:09:58 +0200

    feat: functions for creating accomodation

--- reservation-service/src/main/java/com/reservation/reservationservice/controller/AccomodationController.java
+++ reservation-service/src/main/java/com/reservation/reservationservice/controller/AccomodationController.java
@@ -1,10 +1,29 @@
 package com.reservation.reservationservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.reservation.reservationservice.dtos.AccomodationDTO;
+import com.reservation.reservationservice.exceptions.BadRequestException;
+import com.reservation.reservationservice.service.AccomodationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/accomodation")
 public class AccomodationController {
+
+    @Autowired
+    private AccomodationService accomodationService;
+    private static final String ENTITY_NAME = "accomodation";
+    private static final String applicationName = "reservation service";
+
+    @ResponseBody
+    @PostMapping(path = "/addAccomodation")
+    @ResponseStatus(HttpStatus.OK)
+    public AccomodationDTO saveAccomodation (@RequestBody AccomodationDTO accomodation) throws Exception{
+        if(accomodation.getId()!=null){
+            throw new BadRequestException("Accomodation already have id.");
+        }
+        return  accomodationService.save(accomodation);
+    }
 
 }
