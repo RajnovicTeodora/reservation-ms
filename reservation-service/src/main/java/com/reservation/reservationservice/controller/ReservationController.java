8ce6dee0525c6b319db30d97b37ac0661085fc03
Rajtarov Natasa <rajtarovnatasa@gmail.com>
2023-06-03 10:04:04 +0200

    feat: canceling reservation

--- reservation-service/src/main/java/com/reservation/reservationservice/controller/ReservationController.java
+++ reservation-service/src/main/java/com/reservation/reservationservice/controller/ReservationController.java
@@ -1,9 +1,20 @@
 package com.reservation.reservationservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.reservation.reservationservice.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/reservation")
 public class ReservationController {
+    @Autowired
+    private ReservationService reservationService;
+
+    @ResponseBody
+    @PutMapping(path = "/cancelReservation")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean cancelReservation (@RequestBody String id) throws Exception{
+        return  reservationService.cancelReservation(id);
+    }
 }
